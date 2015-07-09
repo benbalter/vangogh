d3c4c191ce9d4bef5eb076749ffd2f13763e149d
Ben Balter <ben.balter@github.com>
2015-07-09 09:30:59 -0400

    add CSV parsing

--- spec/vangogh_bin_spec.rb
+++ spec/vangogh_bin_spec.rb
@@ -8,4 +8,18 @@ describe "Vangogh::Bin" do
   it "abbreviates values" do
     expect(`bundle exec vangogh abbreviate 1234`.strip).to eql("1.2k")
   end
+
+  it "abstracts CSVs" do
+    expect(`bundle exec vangogh abstract spec/fixtures/csv.csv`.strip).to match(/baz\,12000$/)
+  end
+
+  it "abbreviates CSVs" do
+    expect(`bundle exec vangogh abbreviate spec/fixtures/csv.csv`.strip).to match(/baz\,12k$/)
+  end
+
+  it "respects the field flag" do
+    expect(`bundle exec vangogh abbreviate spec/fixtures/csv.csv --fields foo`.strip).to match(/baz\,12345$/)
+
+    expect(`bundle exec vangogh abbreviate spec/fixtures/csv.csv --fields bar`.strip).to match(/baz\,12k$/)
+  end
 end
